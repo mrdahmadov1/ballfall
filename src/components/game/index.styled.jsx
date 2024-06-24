7eacb4c0bcb2d2452cc64068ddb44b59e783fc87
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-06-24 09:08:15 +0400

    - delete unused files

--- src/components/game/index.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const GameContainer = styled.div`
-  width: 100vw;
-  min-height: 100vh;
-  background: #e4e4e4;
-  position: relative;
-  /* overflow: hidden; */
-`;

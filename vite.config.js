6aca3883809a6c30272f80e14a8d0354877ce040
DESKTOP-KTIOEGP\Murad Ahmadov <murad.ahmadov@vabiss.com>
2024-06-24 06:05:51 +0400

    - reset

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  base: '/ballfall/',
+  build: {
+    outDir: './docs',
+  },
+  base: './',
 });
